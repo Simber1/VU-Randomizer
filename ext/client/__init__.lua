b4ebc24738dab1dc05217eb9186e3fcfba449ba9
Peter <simber2@btinternet.com>
2020-10-06 15:02:10 +0100

    Started work on UI and added vuic files to gitignore

--- ext/client/__init__.lua
+++ ext/client/__init__.lua
@@ -1,1 +1,21 @@
 print("Randomizer Mod Loaded")
+class 'RandomizerClient'
+
+function RandomizerClient:__init()
+    print('Hello world!')
+    print(MyModVersion)
+ 
+    self:RegisterVars()
+    self:RegisterEvents()
+end
+
+function RandomizerClient:RegisterEvents()
+    Events:Subscribe('Extension:Loaded',self, self.StartUI) --Loading webui
+    print("RegisterEvents running")
+end
+
+
+function RandomizerClient:StartUI()
+    WebUI:Init() 
+    WebUI.Call('Show')
+end
